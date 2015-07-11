ff8a84bc33906634980a5b3067fde710fc242d0c
Chris Cornutt <enygma@phpdeveloper.org>
2015-07-11 10:25:36 -0500

    adding handlebars if_eq helper and modifying collections to extend a base

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,12 @@
 // $(function() {
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b) // Or === depending on your needs
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+//----------
+
 	var Group = Backbone.Model.extend({
 		urlRoot: '/group',
 		defaults: {
@@ -34,12 +42,7 @@
 		}
 	});
 
-	var GroupCollection = Backbone.Collection.extend({
-		model: Group,
-		url: '/group'
-	});
-	var PermissionCollection = Backbone.Collection.extend({
-		model: Permission,
+	var BaseCollection = Backbone.Collection.extend({
 		initialize: function(data) {
 			this.data = (typeof data == 'undefined') ? {} : data;
 
@@ -47,59 +50,63 @@
 			this.listenTo(this, 'reset add change', this.updateUrl, this);
 		},
 		url: function() {
-			switch(this.data.type) {
+			return this.buildUrl(this.data);
+		}
+	});
+
+	var GroupCollection = BaseCollection.extend({
+		model: Group,
+		// url: '/group'
+		buildUrl: function(data) {
+			switch(data.type) {
 				case 'user':
-					var url = '/user/'+this.data.userId+'/permission'; break;
+					var url = '/user/'+data.userId+'/group'; break;
+				case 'permission':
+					var url = '/permission/'+data.groupId+'/group'; break;
+				default: var url = '/group'
+			}
+			return url;
+		},
+		updateUrl: function() {
+			this.each(function(perm) {
+				perm.urlRoot = this.buildUrl(this.data);
+			}, this);
+		}
+	});
+	var PermissionCollection = BaseCollection.extend({
+		model: Permission,
+		buildUrl: function(data) {
+			switch(data.type) {
+				case 'user':
+					var url = '/user/'+data.userId+'/permission'; break;
 				case 'group':
-					var url = '/group/'+this.data.groupId+'/permission'; break;
+					var url = '/group/'+data.groupId+'/permission'; break;
 				default: var url = '/permission'
 			}
 			return url;
 		},
 		updateUrl: function() {
-			var data = this.data;
 			this.each(function(perm) {
-				switch(data.type) {
-					case 'user':
-						var url = '/user/'+data.userId+'/permission'; break;
-					case 'group':
-						var url = '/group/'+data.groupId+'/permission'; break;
-					default: var url = '/permission'
-				}
-				perm.urlRoot = url;
-			});
+				perm.urlRoot = this.buildUrl(this.data);
+			}, this);
 		}
 	});
-	var UserCollection = Backbone.Collection.extend({
+	var UserCollection = BaseCollection.extend({
 		model: User,
-		initialize: function(data) {
-			this.data = (typeof data == 'undefined') ? {} : data;
-
-			this.listenTo(this, 'reset add change remove', this.updateUrl, this);
-			this.listenTo(this, 'reset add change', this.updateUrl, this);
-		},
-		url: function() {
-			if (typeof this.data.groupId !== 'undefined') {
-				url = '/group/'+this.data.groupId+'/user';
-			} else if (typeof this.data.userId !== 'undefined') {
-				url = '/permission/'+this.data.userId+'/user';
+		buildUrl: function(data) {
+			if (typeof data.groupId !== 'undefined') {
+				url = '/group/'+data.groupId+'/user';
+			} else if (typeof data.userId !== 'undefined') {
+				url = '/permission/'+data.userId+'/user';
 			} else {
 				url = '/user';
 			}
 			return url;
 		},
 		updateUrl: function() {
-			var data = this.data;
-			this.each(function(perm) {
-				switch(data.type) {
-					case 'user':
-						var url = '/user/'+data.userId+'/user'; break;
-					case 'group':
-						var url = '/group/'+data.groupId+'/user'; break;
-					default: var url = '/permission'
-				}
-				perm.urlRoot = url;
-			});
+			this.each(function(user) {
+				user.urlRoot = this.buildUrl(this.data);
+			}, this);
 		}
 	});
 // });
